e760f5f023e7144aee5e9d020324a814c06c0127
yioo_2007 <yioo_2007@126.com>
2022-03-15 16:48:10 +0800

    modify flask

--- flask-test/mcore.py
+++ flask-test/mcore.py
@@ -1,5 +1,5 @@
 from email.policy import default
-from flask import render_template, request, jsonify, Blueprint, url_for
+from flask import render_template, request, jsonify, Blueprint, url_for, g
 from flask.views import MethodView, View
 import peewee as pw
 
@@ -10,23 +10,35 @@ db = pw.SqliteDatabase('ui/memory.db')
 class BaseView(MethodView):
     _ormCls = None
 
-    def list(self):
+    def before(self, name, *args, **kwargs):
+        orm = self.__class__._ormCls
+        if name == 'list':
+            g.datas = [d.__data__ for d in orm.select().execute()]
+        elif name == 'detail' or name == 'update':
+            d = orm.get_or_none(orm.id == request.args.get('id', type=int))
+            g.data = d.__data__ if d else orm().__data__
+        pass
+
+    def list(self, *args, **kwargs):
         url = f'{self.__class__._url_prefix}/list.html'
-        print('call list-->', url)
         return render_template(url)
 
-    def add(self):
+    def add(self, *args, **kwargs):
         return render_template(f'{self.__class__._url_prefix}/add.html')
 
-    def update(self):
+    def update(self, *args, **kwargs):
         return render_template(f'{self.__class__._url_prefix}/update.html')
+
+    def detail(self, *args, **kwargs):
+        return render_template(f'{self.__class__._url_prefix}/detail.html')
 
     @classmethod
     def as_uiview(cls, funcName):
         def uiview(*args, **kwargs):
             obj = uiview._cls()
             v = getattr(obj, uiview._funcName)
-            return v()
+            obj.before(uiview._funcName, *args, **kwargs)
+            return v(*args, **kwargs)
         uiview._cls = cls
         uiview._funcName = funcName
         uiview.__name__ = cls.__name__ + '_uiview_' + funcName
@@ -43,44 +55,45 @@ class BaseView(MethodView):
         bp.add_url_rule('/list.html', view_func=cls.as_uiview('list'),  methods=['GET',])
         bp.add_url_rule('/add.html', view_func=cls.as_uiview('add'),  methods=['GET',])
         bp.add_url_rule('/update.html', view_func=cls.as_uiview('update'),  methods=['GET',])
+        bp.add_url_rule('/detail.html', view_func=cls.as_uiview('detail'),  methods=['GET',])
+
+    @classmethod
+    def addUrlRule(cls, bp : Blueprint, rule, funcName):
+        bp.add_url_rule(rule, view_func=cls.as_uiview(funcName),  methods=['GET',])
 
     @property
     def orm(self) -> pw.Model:
         return self.__class__._ormCls
 
     def success(self, data):
-        if isinstance(data, list):
-            for d in data:
-                if '_del_tag' in d:
-                    del d['_del_tag']
-        if isinstance(data, dict):
-            if '_del_tag' in data:
-                del data['_del_tag']
         return jsonify(status='OK', msg=None, data=data)
     def fail(self, msg, data = None):
         return jsonify(status='Fail', msg=msg, data=data)
 
+    def get_json(self, name):
+        return request.get_json(force=True, silent=True)
+
     def get(self, id = None):
         if id is None:
-            params = request.get_json(force=True, silent=True)
+            params = self.get_json('get')
             pageIdx = 0
             pageSize = 20
             if params and 'pageIdx' in params:
                 pageIdx = params.pageIdx
             if params and 'pageSize' in params:
                 pageSize = params.pageSize
-            sl = self.orm.select().where(self.orm._del_tag == False).paginate(pageIdx + 1, pageSize)
+            sl = self.orm.select().paginate(pageIdx + 1, pageSize)
             #print(sl.sql())
             val = [u.__data__  for u in sl.execute()]
             return self.success(val)
         else:
-            u = self.orm.get_or_none(self.orm.id == id, self.orm._del_tag == False)
+            u = self.orm.get_or_none(self.orm.id == id)
             if u is not None:
                 return self.success(u.__data__)
             return self.fail(f'Not found by ID of {id}')
 
     def post(self):
-        params = request.get_json(force=True, silent=True)
+        params = self.get_json('post')
         if isinstance(params, list):
             r = []
             for p in params:
@@ -94,7 +107,7 @@ class BaseView(MethodView):
 
     def put(self, id):
         u = self.orm.get_or_none(self.orm.id == id)
-        params = request.get_json(force=True, silent=True)
+        params = self.get_json('put')
         if u is None:
             return self.fail( msg = f'Not found by ID of {id}')
         for k in params:
@@ -104,10 +117,9 @@ class BaseView(MethodView):
         return self.success(None)
 
     def delete(self, id):
-        self.orm.update(_del_tag = True).where(self.orm.id == id).execute()
+        self.orm.delete().where(self.orm.id == id).execute()
         return self.success(None)
 
 class BaseModel(pw.Model):
-    _del_tag = pw.BooleanField(default=False)
     class Meta:
         database = db
