03117172a42e939426e8824ed25f34ed24e00102
yioo_2007 <yioo_2007@126.com>
2023-03-23 16:53:48 +0800

    add Thread

---
+++ XueXi/extension/thread.js
@@ -0,0 +1,84 @@
+function Task(callbackFunc, delay) {
+	this.callbackFunc = callbackFunc;
+	this.delay = delay || 0;
+}
+
+function Thread() {
+	this.tasks = [];
+	this.id = 0;
+	this.curTask = null;
+	this.curTaskBeginTime = 0;
+}
+
+Thread.prototype.start = function() {
+	if (this.id != 0) {
+		return;
+	}
+	let thiz = this;
+	function wrapRun() {
+		thiz._run();
+	}
+	this.curTaskBeginTime = Date.now();
+	this.id = setInterval(wrapRun, 100);
+}
+
+Thread.prototype.pause = function() {
+	
+}
+
+Thread.prototype._run = function() {
+	if (this.curTask != null) {
+		return;
+	}
+	if (this.tasks.length == 0) {
+		return;
+	}
+	let topTask = this.tasks[0];
+	let diffTime = Date.now() - this.curTaskBeginTime;
+	if (diffTime < topTask.delay) {
+		console.log('Wait...');
+		// wait
+		return;
+	}
+	this.curTask = this.tasks.shift();
+	let thiz = this;
+	function _resolve_() {
+		thiz._resolve();
+	}
+	function _reject_() {
+		thiz._reject();
+	}
+	this.curTask.callbackFunc(_resolve_, _reject_);
+}
+
+Thread.prototype._resolve = function() {
+	this.curTask = null;
+	this.curTaskBeginTime = Date.now();
+}
+
+Thread.prototype._reject = function() {
+	this.curTask = null;
+	this.curTaskBeginTime = Date.now();
+	this.tasks.splice(0, this.tasks.length);
+}
+
+Thread.prototype.stop = function() {
+	clearInterval(this.id);
+	this.id = 0;
+	this.curTask = null;
+	this.curTaskBeginTime = 0;
+}
+
+// callbackFunc: function(resolve, reject) 
+// when finish call resolve() or reject()
+Thread.prototype.addTask = function(callbackFunc, delay) {
+	let task = new Task(callbackFunc, delay);
+	this.tasks.push(task);
+}
+
+var NT = new Thread();
+NT.addTask(function(r, v) { setTimeout( function() {console.log('Timeotut Run===AAAA');  r();  }, 10000 );  console.log('ABCD===AAA');   }, 4000);
+NT.addTask(function(r, v) { setTimeout( function() {console.log('Timeotut Run===BBB');  r();  }, 10000 );  console.log('ABCD===BBBB');   }, 4000);
+NT.addTask(function(r, v) { setTimeout( function() {console.log('Timeotut Run===CCC');  v();  }, 10000 );  console.log('ABCD===CCC');   }, 4000);
+NT.addTask(function(r, v) { setTimeout( function() {console.log('Timeotut Run===DDDD');  r();  }, 10000 );  console.log('ABCD===DDDD');   }, 4000);
+NT.start();
